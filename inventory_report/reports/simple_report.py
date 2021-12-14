90be94eb88257cad1b9ba129c762a77c4c4da729
Lucas Ribeiro Barbosa <lucasrb04@gmail.com>
2021-12-14 00:04:18 -0300

    Corrigindo req 1.2 e 1.4

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -5,7 +5,7 @@ class SimpleReport:
     @staticmethod
     def __diff_date_from_now(date_str):
         date_dt = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
-        return (date_dt - datetime.date.today()).days
+        return abs((date_dt - datetime.date.today()).days)
 
     def __count_products(cp_name, product_list):
         count_products = 0
@@ -16,9 +16,9 @@ class SimpleReport:
 
     def __format_report(old_fab, valid_date, cp_name):
         return (
-            f"Data de fabricação mais antiga: {old_fab} \n"
+            f"Data de fabricação mais antiga: {old_fab}\n"
             f"Data de validade mais próxima: {valid_date}\n"
-            f"Empresa com maior quantidade de produtos estocados: {cp_name}"
+            f"Empresa com maior quantidade de produtos estocados: {cp_name}\n"
         )
 
     @classmethod
@@ -33,13 +33,14 @@ class SimpleReport:
         for product in product_list:
             if product["data_de_fabricacao"] < old_fab:
                 old_fab = product["data_de_fabricacao"]
+
             product_diff_time = cls.__diff_date_from_now(
                 product["data_de_validade"]
             )
-
             if product_diff_time < diff_time:
                 diff_time = product_diff_time
                 valid_date = product["data_de_validade"]
+
             count_products = cls.__count_products(
                 product["nome_da_empresa"], product_list
             )

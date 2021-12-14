845e0315b314f87b93ca7dd096f3fec34b4ade38
Lucas Ribeiro Barbosa <lucasrb04@gmail.com>
2021-12-14 00:11:16 -0300

    refatorando

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -14,32 +14,32 @@ class SimpleReport:
                 count_products += 1
         return count_products
 
-    def __format_report(old_fab, valid_date, cp_name):
+    def __format_report(oldest, newest, cp_name):
         return (
-            f"Data de fabricação mais antiga: {old_fab}\n"
-            f"Data de validade mais próxima: {valid_date}\n"
+            f"Data de fabricação mais antiga: {oldest}\n"
+            f"Data de validade mais próxima: {newest}\n"
             f"Empresa com maior quantidade de produtos estocados: {cp_name}\n"
         )
 
     @classmethod
     def generate(cls, product_list):
-        old_fab = product_list[0]["data_de_fabricacao"]
-        valid_date = product_list[0]["data_de_validade"]
+        oldest = product_list[0]["data_de_fabricacao"]
+        newest = product_list[0]["data_de_validade"]
         diff_time = cls.__diff_date_from_now(
             product_list[0]["data_de_validade"]
         )
         cp_name = product_list[0]["nome_da_empresa"]
         count_total = 0
         for product in product_list:
-            if product["data_de_fabricacao"] < old_fab:
-                old_fab = product["data_de_fabricacao"]
+            if product["data_de_fabricacao"] < oldest:
+                oldest = product["data_de_fabricacao"]
 
             product_diff_time = cls.__diff_date_from_now(
                 product["data_de_validade"]
             )
             if product_diff_time < diff_time:
                 diff_time = product_diff_time
-                valid_date = product["data_de_validade"]
+                newest = product["data_de_validade"]
 
             count_products = cls.__count_products(
                 product["nome_da_empresa"], product_list
@@ -47,4 +47,4 @@ class SimpleReport:
             if count_products > count_total:
                 count_total = count_products
                 cp_name = product["nome_da_empresa"]
-        return cls.__format_report(old_fab, valid_date, cp_name)
+        return cls.__format_report(oldest, newest, cp_name)

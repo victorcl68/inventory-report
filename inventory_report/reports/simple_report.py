e85cae47ae189ef87f50aede645ae6b673331fb0
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-12-27 05:09:57 -0300

    update variable names

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -7,29 +7,34 @@ class SimpleReport:
         date_dt = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
         return abs((date_dt - datetime.date.today()).days)
 
-    def __count_products(cp_name, product_list):
-        count_products = 0
+    @staticmethod
+    def __product_counter(company, product_list):
+        product_counter = 0
         for product in product_list:
-            if product["nome_da_empresa"] == cp_name:
-                count_products += 1
-        return count_products
+            if product["nome_da_empresa"] == company:
+                product_counter += 1
+        return product_counter
 
-    def __format_report(oldest, newest, cp_name):
+    @staticmethod
+    def __format_report(oldest, newest, company):
         return (
             f"Data de fabricação mais antiga: {oldest}\n"
             f"Data de validade mais próxima: {newest}\n"
-            f"Empresa com maior quantidade de produtos estocados: {cp_name}\n"
+            f"Empresa com maior quantidade de produtos estocados: {company}\n"
         )
 
     @classmethod
     def generate(cls, product_list):
         oldest = product_list[0]["data_de_fabricacao"]
         newest = product_list[0]["data_de_validade"]
+
         diff_time = cls.__diff_date_from_now(
             product_list[0]["data_de_validade"]
         )
-        cp_name = product_list[0]["nome_da_empresa"]
-        count_total = 0
+
+        company = product_list[0]["nome_da_empresa"]
+
+        total_counter = 0
         for product in product_list:
             if product["data_de_fabricacao"] < oldest:
                 oldest = product["data_de_fabricacao"]
@@ -37,14 +42,16 @@ class SimpleReport:
             product_diff_time = cls.__diff_date_from_now(
                 product["data_de_validade"]
             )
+
             if product_diff_time < diff_time:
                 diff_time = product_diff_time
                 newest = product["data_de_validade"]
 
-            count_products = cls.__count_products(
+            product_counter = cls.__product_counter(
                 product["nome_da_empresa"], product_list
             )
-            if count_products > count_total:
-                count_total = count_products
-                cp_name = product["nome_da_empresa"]
-        return cls.__format_report(oldest, newest, cp_name)
+
+            if product_counter > total_counter:
+                total_counter = product_counter
+                company = product["nome_da_empresa"]
+        return cls.__format_report(oldest, newest, company)
